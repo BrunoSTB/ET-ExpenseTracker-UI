5a43435bf0d726519db1ee08922cf22044a7c54e
Bruno <brunobragast@gmail.com>
2025-05-21 18:28:31 -0300

    added api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUri: '...'
+  apiUri: 'https://et-server-cwhfh6g9apb2b6cw.brazilsouth-01.azurewebsites.net/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUri: '...'
+  apiUri: 'https://et-server-cwhfh6g9apb2b6cw.brazilsouth-01.azurewebsites.net/'
 };
